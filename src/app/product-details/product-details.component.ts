615ad4bee77a596d9d15d565fe5ea4efc6e2d10f
jihwanpapa <pollab@naver.com>
2019-12-17 22:51:08 +0900

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product; //product프로퍼티선언
+  constructor(
+    private route: ActivatedRoute, //컴포넌트 클래스의 생성자에 ActivatedRoute를 의존성으로 주입
+    //ActivatedRoute는 라우터가 컴포넌트를 로드할때 사용한 라우팅 규칭이다.
+  ) { }
 
   ngOnInit() {
+    //라우팅변수를 구독한 후 옵저버에서 productId를 참조한다.
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    });
   }
 
 }
