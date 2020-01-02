e5dd13aec1831fc846b99dbee656e3cc367320bb
jihwanpapa <pollab@naver.com>
2020-01-02 17:39:48 +0900

    tutorial complete

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService} from '../cart.service';
 
 @Component({
@@ -8,10 +9,24 @@ import { CartService} from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items; //제품 목록을 저장할 items 프로퍼티를 선언
+  checkoutForm;
+
   constructor(
-    private cartService: CartService //CartService를 컴포넌트에 의존성으로 주입
+    private cartService: CartService, //CartService를 컴포넌트에 의존성으로 주입
+    private formBuilder: FormBuilder,
   ) { 
     this.items = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData){
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
   ngOnInit() {
